cacab062921d77df6f0cf2ca4ee7ca3266ff6225
Daniel Shepherd <darts100@gmail.com>
2018-09-03 13:28:44 +0800

    Added credits scene

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+	
+	public void LoadCredits()
+	{
+		SceneManager.LoadScene("Credits");
+	}
+	
+	public void LoadMainMenu()
+	{
+		SceneManager.LoadScene("GameScene");
+	}
+}
